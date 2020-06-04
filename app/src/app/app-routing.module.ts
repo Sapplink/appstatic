ab1f97b8e8344cc889407606a601b688c2c0c22f
Marc Atasoy <merih.atasoy@gmail.com>
2020-06-04 16:19:43 -0400

    added ngx/translate as well as landing page updates

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page/landing-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'designs',
+    component: LandingPageComponent
+  },
+  {
+    path: 'projects',
+    component: LandingPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
