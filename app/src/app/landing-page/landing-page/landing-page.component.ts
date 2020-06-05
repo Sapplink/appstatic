f476d2eb6b0e38a25693a2c7d02256d451e97c2c
Marc Atasoy <merih.atasoy@gmail.com>
2020-06-04 23:53:24 -0400

    finalized foundation of landing page

--- app/src/app/landing-page/landing-page/landing-page.component.ts
+++ app/src/app/landing-page/landing-page/landing-page.component.ts
@@ -7,11 +7,25 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
+  hamburger = document.querySelector(".hamburger");
 
   constructor(private translate:TranslateService){
     translate.setDefaultLang('en');
   }
+
   ngOnInit(): void {
+    // On click
+    this.hamburger.addEventListener("click", function() {
+      this.hamburger.classList.toggle("is-active");
+      // add menu opening here
+    });
+  }
+
+  get projectsButtonHover(): string {
+    return this.translate.instant('landingPage.contents.buttons.projectsButtonHover');
+  }
+  get designsButtonHover(): string {
+    return this.translate.instant('landingPage.contents.buttons.designsButtonHover');
   }
 
 }
