93dc367a09a701f44e155c34c85ee3e44dac2e4b
Marc Atasoy <merih.atasoy@gmail.com>
2020-06-05 16:59:58 -0400

    properly updated router module

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Routes } from '../app/app.model'
+import { LandingPageComponent } from './landing-page/landing-page/landing-page.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
-  constructor(public translate: TranslateService) {
-    translate.addLangs(['en']);
-    translate.setDefaultLang('en');
-    translate.use('en');
+export class AppComponent implements OnInit {
+  url: Routes = {
+    path: '/landing-page',
+    component: 'LandingPageComponent'
   }
+  
+  constructor(public translate: TranslateService, public router: Router) {
+  }
+
   title = 'app';
+
+  ngOnInit() {
+    this.setApplicationLanguage()
+    this.navigateTo(this.url);
+  }
+
+  setApplicationLanguage(): void {
+    // To be edited when Turkish is added.
+    this.translate.addLangs(['en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+
+  navigateTo(url: Routes): void {
+    // To be updated when app initializer created
+    this.router.navigate([url.path]);
+  }
 }
