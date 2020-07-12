60382331920d17e18915cf2b5c6690f1bab0bb48
marcmerih <merih.atasoy@gmail.com>
2020-07-12 11:39:10 -0400

    updates with contact and animation

--- app/src/app/contact/contact/contact.component.ts
+++ app/src/app/contact/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../contact.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  contactTypes = [];
+  projectTypes = [];
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor(private contactService: ContactService) {
+    this.formGroup = this.contactService.form;
+    this.contactTypes = [
+      'Project Inquiry',
+      'Networking',
+      'Contracting',
+      'Partnerships'
+    ]
+    this.projectTypes = [
+      'Web Development (UX + Full Stack)',
+      'Graphic Design'
+    ]
+  }
 
   ngOnInit(): void {
   }
