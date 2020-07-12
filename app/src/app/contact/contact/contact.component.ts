a7db83c76240c2eaae193f3d830f2dc7a5bb8ea5
marcmerih <merih.atasoy@gmail.com>
2020-07-12 13:38:52 -0400

    finalized contact page

--- app/src/app/contact/contact/contact.component.ts
+++ app/src/app/contact/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactService } from '../contact.service';
 import { FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
@@ -11,6 +12,7 @@ export class ContactComponent implements OnInit {
   contactTypes = [];
   projectTypes = [];
   formGroup: FormGroup;
+  formSubscriptions: Subscription;
 
   constructor(private contactService: ContactService) {
     this.formGroup = this.contactService.form;
@@ -27,6 +29,21 @@ export class ContactComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log(this.formGroup);
+  }
+  submitForm() {
+    console.log(this.formGroup);
+    if (this.formGroup.get('contactType').value && 
+      this.formGroup.get('projectInquiryType').value &&
+      this.formGroup.get('contactType').value !== 'Project Inquiry') {
+        this.formGroup.get('projectInquiryType').setValue('');
+    }
+    this.contactService.submitForm();
   }
 
+  get shouldDisplayProjectTypes() {
+    if (this.formGroup.get('contactType').value) {
+      return (this.formGroup.get('contactType').value === 'Project Inquiry');
+    }
+  }
 }
